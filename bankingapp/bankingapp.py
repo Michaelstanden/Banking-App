d436597335b8ebd6b8107e79ea199681b67aae3e
Michaelstanden <michaelstanden@hotmail.com>
2021-05-10 17:35:29 +0000

    added functionality for login pages, user detaisl pages and us relogin pages

--- bankingapp/bankingapp.py
+++ bankingapp/bankingapp.py
@@ -26,6 +26,7 @@ def finish_reg():
             new_file.write(age+'\n')
             new_file.write(gender+'\n')
             new_file.write(password+'\n')
+            new_file.write('0')
             new_file.close
             notif.config(fg="green", text="Account Successfully added")
 
@@ -62,21 +63,104 @@ def register():
 
     #Button
     Button(register_screen, text="Register", font=('Calibri', 12)).grid(row=5, stickyN)
+
+
+def login_session():
+    global login_name
+    all_account = os.listdir()
+    login_name = temp_log_name.get()
+    login_password = temp_login_password.get()
+   
+    for name in all_account:
+        if name == login_name:
+            file = open(name, "r")
+            file_data = file.read()
+            file_data = file_data.split('\n')
+            password = file_data[1]
+            #Account Dashborad
+            if login_password == password:
+                login_screen.destroy()
+                account_dashboard = Toplevel(master)
+                account_dashboard.title("Dashboard")
+                #Labels
+                Label(account_dashboard, text="Account Dashboard", font=('Calibri', 12)).grid(row=0, sticky=N, pady=10)
+                Label(account_dashboard, text="Welcome "+name, font=('Calibri', 12)).grid(row=1, sticky=N, pady=5)
+                #Buttons
+                Button(account_dashboard, text="Personal Details", font=('Calibri', 12), width=30, command=personal_details).grid(row=2, sticky=N, padx=10)
+                Button(account_dashboard, text="Deposit", font=('Calibri', 12), width=30, command=deposit).grid(row=3, sticky=N, padx=10)
+                Button(account_dashboard, text="Withdraw", font=('Calibri', 12), width=30, command=withdraw).grid(row=4, sticky=N, padx=10)
+                Label(account_dashboard).grid(row=5, sticky=N, pady=10)
+                return
+            else: 
+                login_notif.config(fg="red", text="Password is incorrect")
+                return
+    login_notif.config(fg="red", text="Account not found")
+
+def deopsit():
+print("deposit")
+
+
+def withdraw():
+    print('withdraw')
+
+def personal_details():
+    #variables
+    file = open(login_name, "r")
+    file_data = file.read()
+    user_details = file_data.split('\n')
+    details_name = user.details[0]
+    details_age = user.detail[2]
+    details_gender = user.details[3]
+    details_balance = user.detail[4]
+    #Personal Details Screen
+    personal_details_screen = Toplevel(master)
+    personal_details_screen.title('Personal Details')
+    print('personal detials')
+    #Labels
+    Label(personal_details_screen, text="Personal Details", font=('Calibri', 12)).grid(row=0, sticky=N, pady=10)
+    Label(personal_details_screen, text="Name : "+details_name, font=('Calibri', 12)).grid(row=1, sticky=W, pady=10)
+    Label(personal_details_screen, text="Age : "+details_age, font=('Calibri', 12)).grid(row=2, sticky=W, pady=10)
+    Label(personal_details_screen, text="Gender : "+details_gender, font=('Calibri', 12)).grid(row=3, sticky=W, pady=10)
+    Label(personal_details_screen, text="Balance : £"+details_balance, font=('Calibri', 12)).grid(row=4, sticky=W, pady=10)
+    
 def login():
-    print('This is a log in page')
+    #Variables
+    global temp_login_name
+    global temp_login_password
+    global login_notif
+    global login_screen
+    temp_login_name = StinrgVar()
+    temp_login_password = StinrgVar()
+    #Login Screen
+    login_screen = TopLevel(master)
+    login_screen.title("Login")
+    Label(login_screen, text="Login to your account", font=('Calibri',12)).grid(row=0, sticky=N, pady10)
+    Label(login_screen, text="Username", font=('Calibri',12)).grid(row=1, sticky=W)
+    Label(login_screen, text="Password", font=('Calibri',12)).grid(row=2, sticky=W)
+    login_notif = Label(login_screen, font=('Calibri', 12))
+    login_notif.grid(row=4, sticky=N)
+
+    #Entries
+    Entry(login_screen, text_variable=temp_login_name).grid(row=1, column=1, padx=5)
+    Entry(login_screen, text_variable=temp_login_password, show="*").grid(row=2, column=1, padx=5)
+
+    #Buttons
+    Button(login_screen text="Login", font('Calibiri', 12), width=20, command=login_session).grid(row=3, sticky=W, pady=5, padx=5)
+
+
 
 #Image Import
 img = Image.open("currency.jpg")
-img = img.resize((150,150))
+img = img.resize((150, 150))
 img = Image.Tk.PhotoImage(img)
 
 #Lables
-Label(master, text  "Custom Banking Beta", font=('Calibri',14)).grid(row=0, sticky=N, pady=10)
-Label(master, text  "Secure Banking online", font=('Calibri',12)).grid(row=1, sticky=N)
+Label(master, text="Custom Banking Beta", font=('Calibri', 14)).grid(row=0, sticky=N, pady=10)
+Label(master, text="Secure Banking online", font=('Calibri', 12)).grid(row=1, sticky=N)
 Label(master, image=img).grid(row=2, sticky=N, pady=15)
 
 #Buttons
-Button(master, text="Register", font('Calibiri'12), width=20, command=register).grid(row=3, sticky=N)
-Button(master, text="Login", font('Calibiri'12), width=20, command=login).grid(row=4, sticky=N, pady=10)
+Button(master, text="Register", font('Calibiri', 12), width=20, command=register).grid(row=3, sticky=N)
+Button(master, text="Login", font('Calibiri', 12), width=20, command=login).grid(row=4, sticky=N, pady=10)
 #added so application can stay in a loop and does not crash
 master.mainloop()
